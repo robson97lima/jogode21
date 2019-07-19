1b7eb4cc7d906ad0e0f4c9d1bb7f435137acf4eb
robson97lima <robson97lima@gmail.com>
2019-07-19 11:52:37 -0300

    Aula303

---
+++ main.py
@@ -0,0 +1,19 @@
+from classes import Carta, Baralho
+
+#carta1 = Carta('Paus', 'As', 1)
+#carta2 = Carta('Copas', 'Rei', 10)
+#carta3 = Carta('Espadas', 'Cinco', 5)
+#baralho = Baralho()
+
+#print(baralho)
+#baralho.adicionar_carta(carta1)
+#baralho.adicionar_carta(carta2)
+#baralho.adicionar_carta(carta3)
+#print(baralho)
+#print(baralho.remover_carta())
+
+baralho = Baralho()
+
+print (baralho.sortear())
+print (baralho.sortear())
+print (baralho.sortear())
